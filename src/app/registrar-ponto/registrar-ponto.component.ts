09db525de2b1b239d00608c99d6170646c7ed46e
almeidavini <vinicius2016@gmail.com>
2022-11-06 17:19:24 -0300

    Gerando data e hora dinamicamente

--- src/app/registrar-ponto/registrar-ponto.component.ts
+++ src/app/registrar-ponto/registrar-ponto.component.ts
@@ -8,17 +8,44 @@ import { Router } from '@angular/router';
   styleUrls: ['./registrar-ponto.component.css']
 })
 export class RegistrarPontoComponent implements OnInit {
+  data: string = this.dataHora();
 
   constructor(private _snackBar: MatSnackBar, private router: Router) { }
 
   ngOnInit(): void {
   }
-
+  
   openSnackBar() {
     this._snackBar.open('Marcação de ponto realizada com sucesso!', 'Fechar',{
       duration: 3000
     });
 
     this.router.navigate(['/', 'home-admin'])
+  }
+
+  dataHora() {
+    var data = new Date();
+    var dia: any = data.getDate();
+    var mes: any = data.getMonth() + 1;
+    var horas: any = new Date().getHours();
+    var minutos: any = new Date().getMinutes();
+
+    if(dia < 10){
+      dia = "0" + dia;
+    }
+
+    if (mes < 10) {
+       mes = "0" + mes;
+    }
+
+    if (horas < 10) {
+       horas = "0" + horas;
+    }
+
+    if (minutos < 10) {
+       minutos = "0" + minutos;
+    }
+
+    return `${dia}-${mes} ${horas}:${minutos}`;
   }
 }
